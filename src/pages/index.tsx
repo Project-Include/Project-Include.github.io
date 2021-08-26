ac50e95effbab5abdd37bfe5d9c79aeffbf30f34
ShabazBadshah <badshah.shabaz.dev@gmail.com>
2021-08-25 23:24:57 -0400

    feat: implemented NavLink component to route between pages/hyperlinks

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,8 @@ import Image from 'next/image';
 
 import styles from '@/styles/Home.module.css';
 
+import NavLink from '@components/NavLink';
+
 export default function Home() {
   return (
     <div className={styles.container}>
@@ -16,6 +18,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
+        <NavLink href="/test">Test</NavLink>
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
